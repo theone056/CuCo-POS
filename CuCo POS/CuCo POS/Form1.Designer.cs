b80561b426b50cfe61395658c810f5be9ddcedd2
theone056 <32253665+theone056@users.noreply.github.com>
2022-02-05 14:40:29 +0800

    Resize Windows Form

--- CuCo POS/CuCo POS/Form1.Designer.cs
+++ CuCo POS/CuCo POS/Form1.Designer.cs
@@ -35,7 +35,7 @@ namespace CuCo_POS
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(776, 369);
+            this.ClientSize = new System.Drawing.Size(706, 333);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
