bf68b3f526c260085eb1ee3590dc0738502eee66
theone056 <32253665+theone056@users.noreply.github.com>
2022-02-17 22:20:56 +0800

    add db sets

--- CuCo POS/CuCo POS/CuCoPOS.cs
+++ CuCo POS/CuCo POS/CuCoPOS.cs
@@ -21,6 +21,7 @@ namespace CuCo_POS
         {
         }
 
+
         // Add a DbSet for each entity type that you want to include in your model. For more information 
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
         public DbSet<MenuList> MenuLists { get; set; }
